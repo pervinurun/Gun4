1ba8902f787d47588fc084843f02ec1acc2ce59a
Pervin Ûrûn <prvn.urun7@outlook.com>
2021-01-28 21:03:40 +0300

    Add project files.

---
+++ Gun4/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gun4
+{
+    class MyDictionary <Key,Value>
+    {
+        Key[] keys;
+        Value[] values;
+
+        public MyDictionary() //Constructor
+        {
+            keys = new Key[0];
+            values = new Value[0];
+
+        }
+        public void Add(Key key,Value value)
+        {
+            Key[] tempKey = keys; //geçici dizi
+            keys = new Key[keys.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+
+            }
+            keys[keys.Length - 1] = key;
+            Value[] tempValue = values;
+            values = new Value[values.Length + 1];
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                values[i] = tempValue[i];
+            }
+            values[values.Length - 1] = value;
+            Console.WriteLine(" TELEFON KODU : " + key + " , " + " Ülke : " + value);
+
+
+
+        }
+
+
+
+    }
+}
---
+++ Gun4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gun4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> sehirler = new MyDictionary<int, string>();
+            sehirler.Add(0905, "TURKEY");
+            sehirler.Add(+1, "AMERİCA");
+            sehirler.Add(+7, "İzmir");
+        }
+    }
+}
